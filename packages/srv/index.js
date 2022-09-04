a8270b4b3d523dab888b669503bb3ef2b9b980cd
Equan Pr <equan.p@gmail.com>
2022-09-04 21:18:33 +0700

    Simple root route

--- packages/srv/index.js
+++ packages/srv/index.js
@@ -6,9 +6,15 @@ dotenv.config();
 const app = express();
 const SRV_PORT = process.env.TELP_PORT || 3003;
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log(`request: ${req.url}`);
-  res.end(`${process.versions?.node}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("root");
 });
 
 http
