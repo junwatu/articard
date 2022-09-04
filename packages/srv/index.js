a7490222210a0bbfe7b3625a92063fe18c0d449f
Equan Pr <equan.p@gmail.com>
2022-09-04 10:22:27 +0700

    Simple express server  accept all routes

---
+++ packages/srv/index.js
@@ -0,0 +1,12 @@
+import http from 'node:http'
+import express from 'express'
+
+const app = express()
+const PORT = 3003
+
+app.use((req, res) => {
+    console.log(`request: ${req.url}`)
+    res.end(`${process.versions?.node}`)
+})
+
+http.createServer(app).listen(PORT, () => console.log(`srv ok port: ${PORT}`))
