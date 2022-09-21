21cd6b810e62990755354ac471f7ca76f3139f5f
Equan Pr <equan.p@gmail.com>
2022-09-21 10:51:19 +0700

    api source configs

--- packages/srv/config.js
+++ packages/srv/config.js
@@ -6,6 +6,19 @@ const telpConfig = {
   app: {
     port: process.env.TELP_PORT || 3003,
   },
+  sources: {
+    rijksmuseum: {
+      user: process.env.TELP_RIJKSMUSEUM_USER_ID,
+      key: process.env.TELP_RIJKSMUSEUM_API_KEY,
+      usersets: [
+        {
+          base: process.env.TELP_RIJKSMUSEUM_BASE_USERSETS_URL,
+          name: process.env.TELP_RIJKSMUSEUM_USERSET_NAME,
+          url: `${process.env.TELP_RIJKSMUSEUM_BASE_USERSETS_URL}/${process.env.TELP_RIJKSMUSEUM_USER_ID}-${process.env.TELP_RIJKSMUSEUM_USERSET_NAME}?key=${process.env.TELP_RIJKSMUSEUM_API_KEY}&format=json`,
+        },
+      ],
+    },
+  },
 };
 
 export { telpConfig as config };
