c38bea120da2e54565b2a0fd7f7a94d7603a9df4
Equan Pr <equan.p@gmail.com>
2022-10-22 11:13:35 +0700

    try catch await

--- packages/srv/tests/index.test.js
+++ packages/srv/tests/index.test.js
@@ -3,7 +3,12 @@ import { telpServer } from "../index.js";
 
 describe("routes", () => {
   test("/", async () => {
-    const response = await request(telpServer).get("/");
-    expect(response.statusCode).toBe(200);
+    try {
+      const response = await request(telpServer).get("/");
+      expect(response.statusCode).toBe(200);
+    } catch (error) {
+      // eslint-disable-next-line jest/no-conditional-expect
+      expect(error).toMatch('error');
+    }
   });
 });
