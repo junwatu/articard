120ac33e2c1260bbeb8a82b250a1e48195102214
Equan Pr <equan.p@gmail.com>
2022-10-26 16:01:21 +0700

    test for data api

--- packages/srv/__tests__/index.test.js
+++ packages/srv/__tests__/index.test.js
@@ -10,5 +10,15 @@ describe("routes", () => {
       // eslint-disable-next-line jest/no-conditional-expect
       expect(error).toMatch('error');
     }
-  });
+  })
+
+  test("/admin/api/data", async () => {
+    try {
+      const response = await request(telpServer).get("/admin/api/data");
+      expect(JSON.parse(response.text)?.userSet?.count).toEqual(197);
+    } catch (error) {
+      // eslint-disable-next-line jest/no-conditional-expect
+      expect(error).toMatch('error');
+    }
+  })
 });
