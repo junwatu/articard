7ca2613c552ac803cbf42a8198fe0cae65bf739d
Equan Pr <equan.p@gmail.com>
2022-11-01 14:13:04 +0700

    using lib.js and format

--- packages/srv/index.js
+++ packages/srv/index.js
@@ -1,68 +1,73 @@
-import http from "node:http";
-import express from "express";
-import bodyParser from "body-parser";
+import http from 'node:http';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-import { config } from "./config.js";
-import { getAPIData, getRandomData, getDataByID, getArtImage, deleteArtObject, connTelpDB } from "./lib.js"
-import { telpLog } from "./log.js"
+import { config } from './config.js';
+import {
+    getAPIData,
+    getRandomData,
+    getDataByID,
+    getArtImage,
+    deleteArtObject,
+    connTelpDB,
+} from './lib.js';
+import { telpLog } from './log.js';
 
 connTelpDB()
-  .then(() => telpLog.info(`mongodb database ok`))
-  .catch(err => telpLog.error(err))
+    .then(() => telpLog.info(`mongodb database ok`))
+    .catch((err) => telpLog.error(err));
 
 const app = express();
-
 const SRV_PORT = config.app.port;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  telpLog.info(`request: ${req.url}`);
-  next();
+    telpLog.info(`request: ${req.url}`);
+    next();
 });
 
-app.get("/", async (req, res) => {
-  const rData = await getRandomData()
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(rData));
+app.get('/', async (req, res) => {
+    const rData = await getRandomData();
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(rData));
 });
 
-app.get("/api/data/:id", async (req, res) => {
-  telpLog.info(req.params.id)
-  res.statusCode = 200;
+app.get('/api/data/:id', async (req, res) => {
+    telpLog.info(req.params.id);
+    res.statusCode = 200;
 
-  const yourData = await getDataByID(req.params.id)
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(yourData));
-})
+    const yourData = await getDataByID(req.params.id);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(yourData));
+});
 
-app.get("/api/data/image/:id", async (req, res) => {
-  const imageUrl = await getArtImage(req.params.id)
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(`<img src="${imageUrl}" width="50%"/>`);
-})
-
+app.get('/api/data/image/:id', async (req, res) => {
+    const imageUrl = await getArtImage(req.params.id);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<img src="${imageUrl}" width="50%"/>`);
+});
 
 // admin
-app.get("/admin/api/data/delete/:id", async (req, res) => {
-  const isDeleted = await deleteArtObject(req.params.id)
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(isDeleted);
-})
-
-app.get("/admin/api/data", getAPIData);
-
-app.use((req, res) => {
-  res.writeHead(404, { "Content-Type": "application/json" });
-  const errorObj = { code: 404, error: "route not found" };
-  telpLog.error(errorObj);
-  res.end(JSON.stringify(errorObj));
+app.get('/admin/api/data/delete/:id', async (req, res) => {
+    const isDeleted = await deleteArtObject(req.params.id);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(isDeleted);
 });
 
-http
-  .createServer(app)
-  .listen(SRV_PORT, () => telpLog.info(`server port: ${SRV_PORT}`));
+app.get('/admin/api/data', getAPIData);
+
+app.use((req, res) => {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    const errorObj = { code: 404, error: 'route not found' };
+    telpLog.error(errorObj);
+    res.end(JSON.stringify(errorObj));
+});
+
+http.createServer(app).listen(SRV_PORT, () =>
+    telpLog.info(`server port: ${SRV_PORT}`)
+);
 
 export { app as telpServer };
