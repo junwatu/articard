ae77264bbbdbd88fe473ee5cb9237183f3249e68
Equan Pr <equan.p@gmail.com>
2022-11-05 18:47:52 +0700

    add test route for image

--- packages/srv/__tests__/index.test.js
+++ packages/srv/__tests__/index.test.js
@@ -21,4 +21,34 @@ describe('routes', () => {
             expect(error).toMatch('error');
         }
     });
+
+    // http://localhost:3113/api/data/AK-MAK-247
+    test('/api/data/:id', async () => {
+        try {
+            const response = await request(telpServer).get(
+                '/api/data/AK-MAK-247'
+            );
+
+            expect(JSON.parse(response.text)[0]?.links?.artobject).toEqual(
+                'https://www.rijksmuseum.nl/api/en/collection/AK-MAK-247'
+            );
+        } catch (error) {
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(error).toMatch('error');
+        }
+    });
+
+    test('/api/data/image/:id', async () => {
+        try {
+            const response = await request(telpServer).get(
+                '/api/data/image/AK-MAK-247'
+            );
+            expect(response.text).toEqual(
+                `<img src="https://lh4.ggpht.com/mUVNuSSFYLvaO4kjMUuQ11ICGhj3N7o0gA2ZkxSm6GdEq94T2nxXd4wE8h9umTvW0CMsmFZ6t1TPnrdIWeuEfFEIZg=s0" width="50%"/>`
+            );
+        } catch (error) {
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(error).toMatch('error');
+        }
+    });
 });
