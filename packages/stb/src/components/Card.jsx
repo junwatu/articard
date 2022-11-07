60fa20ebf31858a5b45da5823d3e628d0d5790f1
Equan Pr <equan.p@gmail.com>
2022-11-07 21:33:05 +0700

    Card component

---
+++ packages/stb/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
+
+export default function Card() {
+  const getData = useCallback(async () => {
+    const response = await fetch(
+      `http://localhost:3113/api/data/RP-F-2005-159-47`
+    );
+    const data = await response.json();
+    const imgSrc = data[0]?.image?.cdnUrl;
+    setData(imgSrc);
+  }, []);
+
+  const [data, setData] = useState("");
+  useEffect(() => {
+    getData().catch((e) => console.log(e));
+  }, [getData]);
+  return (
+    <div className="App">
+      <div>
+        <img src={data} width="100%" />
+      </div>
+    </div>
+  );
+}
