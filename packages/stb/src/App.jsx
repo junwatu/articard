66f735cc57daf09adfdfba973fc8a795d2072131
Equan Pr <equan.p@gmail.com>
2022-11-07 21:33:25 +0700

    import Card to main App

--- packages/stb/src/App.jsx
+++ packages/stb/src/App.jsx
@@ -1,25 +1,10 @@
-import { useState, useEffect, useCallback } from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const getData = useCallback(async () => {
-    const response = await fetch(
-      `http://localhost:3113/api/data/RP-F-2005-159-47`
-    );
-    const data = await response.json();
-    const imgSrc = data[0]?.image?.cdnUrl;
-    setData(imgSrc);
-  }, []);
-
-  const [data, setData] = useState("");
-  useEffect(() => {
-    getData().catch((e) => console.log(e));
-  }, [getData]);
   return (
     <div className="App">
-      <div>
-        <img src={data} width="100%" />
-      </div>
+      <Card />
     </div>
   );
 }
