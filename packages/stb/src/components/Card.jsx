473e9475d0b25f731c9eed444350487e3159c010
Equan Pr <equan.p@gmail.com>
2022-11-14 15:22:20 +0700

    add image description

--- packages/stb/src/components/Card.jsx
+++ packages/stb/src/components/Card.jsx
@@ -4,11 +4,19 @@ export default function Card() {
   const getData = useCallback(async () => {
     const response = await fetch(`http://localhost:3113/api/data/image/random`);
     const data = await response.json();
-    const imgSrc = data[0]?.image?.cdnUrl;
+    const imgSrc = data.artObject.webImage.url;
+    const title = data.artObject.title;
+    const artNumber = data.artObject.objectNumber;
+
     setData(imgSrc);
+    setDataTitle(title);
+    setArtNumber(artNumber);
   }, []);
 
   const [data, setData] = useState("");
+  const [dataTitle, setDataTitle] = useState("");
+  const [artNumber, setArtNumber] = useState("");
+
   useEffect(() => {
     getData().catch((e) => console.log(e));
   }, [getData]);
@@ -16,6 +24,10 @@ export default function Card() {
     <div className="App">
       <div>
         <img src={data} width="100%" />
+        <div>
+          <p class="title">{dataTitle}</p>
+          <p class="titleDate">{artNumber}</p>
+        </div>
       </div>
     </div>
   );
