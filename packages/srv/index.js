45c3d248a7db97158f8a0e9901633dadf9db25c9
Equan Pr <equan.p@gmail.com>
2022-11-18 13:04:57 +0700

    refactor

--- packages/srv/index.js
+++ packages/srv/index.js
@@ -4,64 +4,54 @@
 import http from 'node:http';
 import express from 'express';
 import bodyParser from 'body-parser';
-import rateLimit from 'express-rate-limit';
-import cors from 'cors';
 import path from 'node:path';
+import cors from 'cors';
 
+import { __dirname } from './utils/fs.js';
 import { config } from './config.js';
-import * as telpCore from './lib.js';
 import { telpLog } from './log.js';
-import * as url from 'url';
-
-//const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-const telpAPIReqLimit = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    message: '100 API request every 15 minute only!',
-    handler: (req, res, next, options) => {
-        telpLog.info(options.message);
-        res.status(options.statusCode).send(options.message);
-    },
-});
+import { userRouter } from './routes/user.js';
+import { adminRouter } from './routes/admin.js';
+import { telpAPIReqLimit } from './utils/ratelimit.js';
+//import * as telpCore from './lib.js';
 
 const app = express();
 const SRV_PORT = config.app.port;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/publik')));
+app.use(express.static(path.join(__dirname, '../publik')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     telpLog.info(`request: ${req.url}`);
     next();
 });
 
-app.use('/api/data', telpAPIReqLimit);
+app.use('/user/api/v1', telpAPIReqLimit);
+app.use('/user/api/v1', userRouter);
 
-// homepage
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/publik/index.html'));
-});
+/**
 app.get('/api/data/random', telpCore.getRandomArt);
 app.get('/api/data/image/random', telpCore.getRandomArtImage);
 app.get('/api/data/:id', telpCore.getArtObjectByID);
 app.get('/api/data/image/:id', telpCore.getImageByID);
 app.get('/api/data/image/s/:id', telpCore.streamArtImage);
 app.get('/api/data/collection/:artObjectNumber', telpCore.getArtDetails);
+*/
+
+app.use('/admin/api/v1', adminRouter);
 
 /**
- * Admin API
- * 🔒🛅🔑
- */
 app.get('/admin/api/data', telpCore.isLoggedIn, telpCore.getAPIData);
 app.get(
     '/admin/api/data/delete/:id',
     telpCore.isLoggedIn,
     telpCore.deleteArtObjectByID
 );
+*/
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/publik/index.html'));
+});
 
 app.use((req, res) => {
     res.writeHead(404, { 'Content-Type': 'application/json' });
