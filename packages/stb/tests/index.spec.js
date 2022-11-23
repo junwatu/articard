01b3a36669904435b5f4b6af0df1acb016b055b2
Equan Pr <equan.p@gmail.com>
2022-11-23 10:15:24 +0700

    fix e2e basic test

--- packages/stb/tests/index.spec.js
+++ packages/stb/tests/index.spec.js
@@ -1,13 +1,9 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("img tag with src", async ({ page }) => {
+test("homepage", async ({ page }) => {
   await page.goto("http://localhost:3113");
   page.reload();
   const artImg = page.getByRole("img");
-  await expect(artImg).toHaveAttribute(
-    "src",
-    RegExp("https://lh*.ggpht|googleusercontent.com/.*")
-  );
-  page.reload();
+  await expect(artImg).toHaveAttribute("src", RegExp("http:/.*user/api/v1.*"));
 });
